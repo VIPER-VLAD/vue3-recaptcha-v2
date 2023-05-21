a7c9bc3fcbfe30499f3ed5d7996de980a50d0283
VIPER <vlad.052898@gmail.com>
2023-05-22 01:43:05 +0600

    feat: v2 invisible support

--- src/useRecaptchar.ts
+++ src/useRecaptchar.ts
@@ -1,5 +1,7 @@
 export interface Recaptcha {
   resetRecaptcha(widgetId: number): void;
+  getResponse(widgetId: number): Promise<string>;
+  execute(widgetId: number): Promise<void>;
 }
 
 export function useRecaptcha(): Recaptcha {
@@ -10,7 +12,23 @@ export function useRecaptcha(): Recaptcha {
     window.grecaptcha.reset(widgetId);
   };
 
+  const getResponse = async (widgetId: number): Promise<string> => {
+    if (typeof widgetId !== "number")
+      throw new Error("getResponse must have 'widgetId'");
+
+    return await window.grecaptcha.getResponse(widgetId);
+  };
+
+  const execute = async (widgetId: number): Promise<void> => {
+    if (typeof widgetId !== "number")
+      throw new Error("execute must have 'widgetId'");
+
+    return await window.grecaptcha.execute(widgetId);
+  };
+
   return {
     resetRecaptcha,
+    getResponse,
+    execute,
   };
 }
